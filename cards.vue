2651a6c17d7dd6eff1f0e5d5cacb030970824425
Ahmad_Hemmati_Zoleyva <ahmadhematiz95@gmail.com>
2023-05-25 07:52:18 +0330

    cards project by Vue and Vuetify

---
+++ cards.vue
@@ -0,0 +1,85 @@
+<<template>
+    <div id="app">
+        <div class="container" id="phones-cards"  >
+          <div class="card" v-for="todo in todos" :key="todo.id" >
+            <img :src="todo.image" alt="sam">
+            <h1>{{ todo.title }}</h1>
+            <p class="price">{{ todo.price}} تومان</p>
+            <p>{{ todo.category }}</p>
+            <button>افزودن به سبد خرید</button>
+          </div>
+       </div>
+    </div>
+</template>
+
+<script>
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      id: 0,
+      todos: [
+        { id: this.id++, text: 'Learn HTML' },
+        { id: this.id++, text: 'Learn JavaScript' },
+        { id: this.id++, text: 'Learn Vue' },
+        { id: this.id++, text: 'Learn Vuetify' }
+      ]
+    }
+  },
+  mounted() {
+   this.getAndShowCards()
+  },
+  methods: {
+    getAndShowCards() {
+      axios.get('https://fakestoreapi.com/products').then((response) => {
+        console.log(response.data);
+        this.todos = response.data;
+      })
+    }
+  }
+});
+</script>
+
+<style>
+.container {
+    text-align: center;
+    direction: rtl;
+
+}
+
+.card {
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.356);
+  display: inline-block;
+  width: 15%;
+  max-height: 400px;
+  padding: 2%;
+  margin: 2%;
+}
+
+img {
+    width: 50%;
+    height: 100px;
+}
+
+h1 {
+    font-size: 15px;
+    height: 50px;
+}
+
+p {
+    height: 50px;
+}
+
+.price {
+    color: teal;
+}
+
+button {
+    width: 80%;
+    height: 50px;
+    background-color: cornflowerblue;
+    color: black;
+    margin: 20px;
+    cursor: pointer;
+}
+</style>
---
+++ cards_vuetify.vue
@@ -0,0 +1,74 @@
+<<template>
+  <!-- <script src="https://unpkg.com/axios/dist/axios.min.js"></script> -->
+
+  <div id="app">
+          <v-app id="inspire">
+                 <v-card
+            class="mx-auto"
+            
+          >
+            <v-container fluid>
+              <v-row dense dir="rtl" class="text-center">
+                <v-col
+                  v-for="card in cards"
+                  :key="card.title"
+                  cols="3"
+                >
+                  <v-card
+                  class="mx-4 mb-4 pa-2"
+                  height="90%"
+                          rounded="lg"
+                  >
+                    <v-img
+                           class="ma-2"
+                      :src="card.image"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="60%"
+                    >
+                     
+                    </v-img>
+              <div class="title text-truncate">{{card.title}}</div>
+              <div class="title text-truncate mt-4" style="color: teal">{{card.price * 50000}} تومان</div>
+  
+              
+        
+              <v-btn class="mt-8 mb-2" color="orange">
+                خرید
+              </v-btn>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+         
+          </v-app>
+        </div>
+</template>
+
+<script>
+new Vue({
+  el: "#app",
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      collapseOnScroll: true,
+      title: "Vue-2 Appssssssss",
+      cards: null
+  
+    };
+  },
+  mounted() {
+   this.getAndShowCards()
+  },
+  
+   methods: {
+    getAndShowCards() {
+      axios.get('https://fakestoreapi.com/products').then((response) => {
+        console.log(response.data);
+        this.cards = response.data;
+      })
+    }
+  }
+});
+
+</script>
