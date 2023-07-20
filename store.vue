8847755cc28da7dbecff01dadaca81911462f65e
Ahmad_Hemmati_Zoleyva <ahmadhematiz95@gmail.com>
2023-07-20 13:46:07 +0330

    store use in options api

---
+++ store.vue
@@ -0,0 +1,30 @@
+<script>
+import { mapState } from "pinia";
+import { useTestPiniaStore } from "@/store";
+
+export default {
+  computed: {
+    ...mapState(useTestPiniaStore, ["name"]),
+    ...mapState(useTestPiniaStore, ["job"]),
+  },
+};
+</script>
+
+<template>
+  <pre>name: {{ job }}</pre>
+</template>
+
+<!-- import { createPinia, defineStore } from 'pinia'
+
+export const useTestPiniaStore = defineStore('testStore', {
+  state: () => {
+    return {
+      name: 'bob',
+      job: 'programer',
+    }
+  },
+})
+
+const store = createPinia()
+
+export default store -->
